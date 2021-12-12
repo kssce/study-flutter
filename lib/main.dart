329cee2196c7e52bc00d1b3bf57b30f2b0000cbb
kssce <kssce7@gmail.com>
2021-12-13 06:34:19 +0900

    add assets and images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intro_flutter/ui/assets_and_image.dart';
 import 'package:intro_flutter/ui/box_constraints.dart';
 import 'package:intro_flutter/ui/interactive_study.dart';
 import 'package:intro_flutter/ui/introduction.dart';
@@ -16,4 +17,5 @@ import 'ui/state_example.dart';
 // void main() => runApp(const LayoutTutorial());
 // void main() => runApp(const Responsive());
 // void main() => runApp(const MyBoxConstraints());
-void main() => runApp(const InteractiveExample());
+// void main() => runApp(const InteractiveExample());
+void main() => runApp(AssetsAndImages());
